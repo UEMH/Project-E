e7e9fe0dfec149e64d1c1c9359b9d7cdfd0b27df
UEMH <uemh1520@gmail.com>
2025-11-23 10:45:58 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: [true, '用户名不能为空'],
     unique: true,
     trim: true,
-    minlength: 3,
-    maxlength: 30
+    minlength: [3, '用户名至少需要3个字符'],
+    maxlength: [30, '用户名不能超过30个字符']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6
+    required: [true, '密码不能为空'],
+    minlength: [6, '密码至少需要6个字符']
   },
   createdAt: {
     type: Date,
@@ -20,6 +21,23 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// 移除 correctPassword 方法，因为我们在 auth.js 中直接使用 bcrypt
+// 在保存前加密密码
+userSchema.pre('save', async function(next) {
+  // 只有当密码被修改时才加密
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 添加密码验证方法
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 module.exports = mongoose.model('User', userSchema);
