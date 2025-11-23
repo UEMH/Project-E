7512d789401b428e320b616ff7d0ed8bb980893a
UEMH <uemh1520@gmail.com>
2025-11-23 09:08:32 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,21 +4,24 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: [true, '用户名不能为空'],
     unique: true,
-    trim: true
+    trim: true,
+    minlength: [3, '用户名至少3个字符'],
+    maxlength: [30, '用户名不能超过30个字符']
   },
   email: {
     type: String,
-    required: true,
+    required: [true, '邮箱不能为空'],
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, '请输入有效的邮箱地址']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6
+    required: [true, '密码不能为空'],
+    minlength: [6, '密码至少6个字符']
   },
   createdAt: {
     type: Date,
@@ -26,16 +29,30 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// 密码加密
+// 密码加密中间件
 userSchema.pre('save', async function(next) {
+  // 只有在密码被修改时才加密
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-// 密码验证
+// 密码验证方法
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+// 移除密码字段从JSON输出
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
 };
 
 module.exports = mongoose.model('User', userSchema);
